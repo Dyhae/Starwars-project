4408f32539eb4843eaad895b231a6869192dadaf
doulabi <dyhaeoulabi@outlook.com>
2020-02-26 15:48:30 -0600

    Created the starship controller and index and the show

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class StarshipsController < ApplicationController
   def index
-
+    @starships = Starship.all.order(:name)
   end
 
   def show
-
+    @starship = Starship.find(params[:id])
   end
 end
