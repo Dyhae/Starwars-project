36890c671cdcbf300639ac22e950df5170be290b
doulabi <dyhaeoulabi@outlook.com>
2020-02-26 15:43:05 -0600

    Created the controller and the index and the show

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class KindsController < ApplicationController
   def index
-    
+    @kinds = Kind.includes(:planet).order(:name)
   end
 
   def show
-
+    @kind = Kind.find(params[:id])
   end
 end
